6f17cabccce35680177b2dac3ade6c62beadd27e
Liron Zvi <t-lironzvi@microsoft.com>
2022-05-02 20:13:44 +0300

    Maze constuctors

--- src/algorithms/mazeGenerators/Maze.java
+++ src/algorithms/mazeGenerators/Maze.java
@@ -1,4 +1,22 @@
 package algorithms.mazeGenerators;
 
+import javax.swing.text.Position;
+
 public class Maze {
+    int numRows;
+    int numColumns;
+    Position startP;
+    Position endP;
+
+    public Maze(int rows, int columns, Position startP, Position endP ){
+        this.startP =startP;
+        this.endP = endP;
+        this.numRows = rows;
+        this.numColumns = columns;
+    }
+
+    public Maze(int rows, int columns){
+        this.numRows = rows;
+        this.numColumns = columns;
+    }
 }
--- src/algorithms/mazeGenerators/SimpleMazeGenerator.java
+++ src/algorithms/mazeGenerators/SimpleMazeGenerator.java
@@ -1,12 +1,17 @@
 package algorithms.mazeGenerators;
 
+import java.util.Random;
     public class SimpleMazeGenerator extends AMazeGenerator {
+
     @Override
     public Maze generate(int rows, int columns) {
+        int[][] maze = new int[rows][columns];
+        Random random = new Random();
         for (int i = 0; i < rows; i++) {
-            for (int j = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                maze[i][j] = random.nextInt(2);
             }
         }
-        return new Maze();
+
     }
 }
