ade249c58c5ef48f653c09733bb087badd420753
Liron Zvi <t-lironzvi@microsoft.com>
2022-05-28 12:57:12 +0300

    add function to less thn 2

--- src/algorithms/mazeGenerators/EmptyMazeGenerator.java
+++ src/algorithms/mazeGenerators/EmptyMazeGenerator.java
@@ -4,6 +4,10 @@ public class EmptyMazeGenerator extends AMazeGenerator {
 
     @Override
     public Maze generate(int rows, int columns) {
+        if(rows<2 || columns<2){//if parameters are wrong
+            rows=2;
+            columns=2;
+        }
         Maze emptyMaze = new Maze(rows, columns);
         return emptyMaze;
     }
