615372804513894d8c1e5cc1b61453e413566415
ofirbel1 <104782782+ofirbel1@users.noreply.github.com>
2022-05-28 22:41:21 +0300

    link

---
+++ src/test/GeneralCheckingFunctions.java
@@ -0,0 +1,8 @@
+package test;
+
+public class GeneralCheckingFunctions {
+    public static String getGithubLink(){
+        String githubLink = "https://github.com/lironzvi2/AP-homework";
+        return githubLink;
+    }
+}
